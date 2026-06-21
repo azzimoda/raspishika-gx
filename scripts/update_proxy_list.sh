9f86a9feccd2548570d638578ddbc2a3d9b91728
azzimoda <konstantinmaca@gmail.com>
2026-06-21 11:35:10 +0500

    Update update_proxy_list.sh

--- scripts/update_proxy_list.sh
+++ scripts/update_proxy_list.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+FILE="storage/proxies.json"
 mkdir -p storage
-curl -sL https://cdn.jsdelivr.net/gh/proxifly/free-proxy-list@main/proxies/all/data.json -o storage/proxies.json
+curl -sL https://cdn.jsdelivr.net/gh/proxifly/free-proxy-list@main/proxies/all/data.json -o "$FILE"
+echo "Proxy list updated: $FILE"
