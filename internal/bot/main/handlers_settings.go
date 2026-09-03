946f63fbde1a75a861c18959500d99fb793d5445
azzimoda <konstantinmaca@gmail.com>
2026-09-03 07:05:54 +0500

    refactor(bot): Do not log some errors on deleting user command messages

--- internal/bot/main/handlers_settings.go
+++ internal/bot/main/handlers_settings.go
@@ -24,8 +24,7 @@ func (h *handler) handleCmdSettings(ctx context.Context, b *bot.Bot, update *mod
 	chatID := update.Message.Chat.ID
 	threadID := update.Message.MessageThreadID
 
-	_, err := botutil.DeleteMessage(ctx, b, update.Message)
-	addHandlerCtxErr(ctx, err)
+	botutil.DeleteMessage(ctx, b, update.Message)
 
 	chat, ok := ctx.Value(keyChat).(*model.Chat)
 	if !ok {
@@ -533,8 +532,7 @@ func (h *handler) handleTextGroup(ctx context.Context, b *bot.Bot, update *model
 }
 
 func (h *handler) handleCmdAccess(ctx context.Context, b *bot.Bot, update *models.Update) {
-	_, err := botutil.DeleteMessage(ctx, b, update.Message)
-	addHandlerCtxErr(ctx, err)
+	botutil.DeleteMessage(ctx, b, update.Message)
 
 	chat, ok := ctx.Value(keyChat).(*model.Chat)
 	if !ok {
